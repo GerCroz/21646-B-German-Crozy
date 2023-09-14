f9d8551e79dea2ad877698e7bbf85cf57f9489ba
German <ger.fsa@gmail.com>
2023-09-13 21:39:14 -0300

    creacion del modelo

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,7 +1,20 @@
+import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
 
 const PostModel = sequelize.define("post", { 
-    title:, 
-    content:, 
-    link:,
+    title: {
+        type: DataTypes.STRING, 
+        allowNull: false
+    },
+    content:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    link: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+},
+{
+    timestamps: true
 })
